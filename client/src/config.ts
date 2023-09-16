055713194e2dbca31902fd8380c2f9d8ebf2bb3e
unknown <lethanhphuocdktd@gmail.com>
2023-09-17 01:03:29 +0700

    project 4

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'rzcmo7egih';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-f0g2a2jazpxrpw67.us.auth0.com',
+  clientId: 'm7j7C3xgXFh10NmX1ROpG7ZSlXj2dyiw',
+  callbackUrl: 'http://localhost:3000/callback'
+};
